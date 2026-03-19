93bb23162aa78dccb68c4591301675f5c32124f4
agustin-teixido <afteixido@mail.austral.edu.ar>
2026-03-19 14:06:43 -0400

    Add files via upload

--- exercise_currency.py
+++ exercise_currency.py
@@ -11,3 +11,10 @@ def currency():
     tasa_dolar = 1500  # 1 dólar = 1500 pesos
     tasa_euro = 1600   # 1 euro = 1600 pesos
     tasa_real = 250    # 1 real = 250 pesos
+
+    dolar = pesos / tasa_dolar
+    print(dolar)
+    euro = pesos / tasa_euro
+    print(euro)
+    real = pesos / tasa_real
+    print(real)
